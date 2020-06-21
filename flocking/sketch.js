90ba3b5c9b6a6fae18fdc8f22801fc28e3e2c574
unknown <brychan.thomas@btinternet.com>
2020-06-21 23:05:46 +0100

    Started creating Flocker class

--- flocking/sketch.js
+++ flocking/sketch.js
@@ -1,11 +1,28 @@
 class Flocker {
-  constructor() {
-    
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0,0);
+    this.mass = 1;
+  }
+
+  applyForce(force) {
+    acceleration.add(force.div(this.mass));
+  }
+
+  display() {
+    circle(this.position.x, this.position.y, 10);
+  }
+
+  update() {
+    velocity.add(acceleration);
+    position.add(velocity);
+    acceleration.mult(0);
   }
 }
 
 function setup() {
-
+  createCanvas(800, 500);
 }
 
 function draw() {
